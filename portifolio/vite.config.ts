32174b1772433ea60d01617c2d75216eca865670
Pedro Manoel <pedro.borges@gruporiomais.com.br>
2026-08-12 15:59:32 -0300

    config: prepara projeto para github pages-correção do nome do repositório

--- portifolio/vite.config.ts
+++ portifolio/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio-react/",
+  base: "/portifolio-react/",
   plugins: [react(), tailwindcss()],
 });
